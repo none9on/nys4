4852af9d9a4728c016cf19ed727ddfb5eadff580
none9on <meet.u.in.tokyo@gmail.com>
2025-12-16 12:54:49 +0500

    emailjs instead of web3forms

--- form.js
+++ form.js
@@ -1,171 +1,87 @@
+window.addEventListener("DOMContentLoaded", () => {
+    const form = document.getElementById("survey-form");
+    if (!form) return;
 
+    (function () {
+        emailjs.init("ZIV63XRFDVfzH9a4D");
+    })();
 
-function showSidebar() {
-    const sideBar = document.querySelector('.side-bar');
-    sideBar.style.display = 'flex';
-}
+   
+    const forbiddenChars = /[=<>{}\[\]]/;
 
-function hideSidebar() {
-    const sideBar = document.querySelector('.side-bar');
-    sideBar.style.display = 'none';
-}
+  
+    const fields = form.querySelectorAll("input, textarea");
+    fields.forEach(field => {
+        const saved = localStorage.getItem(field.name);
+        if (saved) field.value = saved;
 
-
-function enableMobileStack() {
-    const container = document.querySelector(".mobile-stack");
-    if (!container) return;
-
-    let photos = Array.from(container.querySelectorAll("img"));
-
-    photos.forEach(img => {
-        img.addEventListener("click", () => {
-            let first = photos.shift();
-            photos.push(first);
-
-           
-            photos.forEach((img, i) => {
-                img.style.zIndex = photos.length - i;
-
-              
-            });
+        field.addEventListener("input", () => {
+            localStorage.setItem(field.name, field.value);
         });
     });
-}
 
+  
+    form.addEventListener("submit", function (e) {
+        e.preventDefault();
 
-if (window.innerWidth <= 540) {
-    enableMobileStack();
-}
-
-const form = document.getElementById('survey-form');
-
-document.querySelectorAll('.next-btn').forEach(button => {
-    button.addEventListener('click', async () => {
-        const currentPart = button.parentElement;
-        const nextPartId = button.getAttribute('data-next');
-        const nextPart = document.getElementById(nextPartId);
-
-        const formData = new FormData();
-        currentPart.querySelectorAll('input, textarea').forEach(input => {
-            if(input.value.trim() !== '') {
-                formData.append(input.name, input.value);
+        for (let f of fields) {
+            if (forbiddenChars.test(f.value)) {
+                alert(`Поле "${f.name}" содержит недопустимые символы! = < > { } [ ] |`);
+                return;
             }
-        });
-        formData.append('access_key', '046ab3ea-c2ac-4eee-a29d-da90b2750ab1');
+        }
 
-        try {
-            const response = await fetch('https://api.web3forms.com/submit', {
-                method: 'POST',
-                body: formData
+        emailjs.sendForm("service_66kuzd8", "template_nktnvh8", this)
+            .then(() => {
+                alert("Анкета отправлена! Спасибо за участие!");
+                form.reset();
+               
+                fields.forEach(f => localStorage.removeItem(f.name));
+            })
+            .catch((error) => {
+                console.error("Ошибка:", error);
+                alert("Ошибка при отправке формы");
             });
-
-            const result = await response.json();
-
-            if (result.success) {
-                currentPart.style.display = 'none';
-                nextPart.style.display = 'block';
-            } else {
-                console.log(result);
-                alert('Ошибка при отправке, проверьте данные формы.');
-            }
-        } catch (error) {
-            console.error(error);
-            alert('Ошибка сети, попробуйте снова.');
-        }
     });
-});
 
-
-form.addEventListener('submit', async (e) => {
-    e.preventDefault();
-    const lastPart = document.getElementById('part3');
-
-    const formData = new FormData();
-    lastPart.querySelectorAll('input, textarea').forEach(input => {
-        if(input.value.trim() !== '') {
-            formData.append(input.name, input.value);
-        }
-    });
-    formData.append('access_key', '046ab3ea-c2ac-4eee-a29d-da90b2750ab1');
-
-    try {
-        const response = await fetch('https://api.web3forms.com/submit', {
-            method: 'POST',
-            body: formData
-        });
-
-        const result = await response.json();
-
-        if (result.success) {
-            document.getElementById('success-message').style.display = 'block';
-            lastPart.querySelector('button[type="submit"]').style.display = 'none';
-        } else {
-            console.log(result);
-            alert('Ошибка при отправке, попробуйте снова');
-        }
-    } catch (error) {
-        console.error(error);
-        alert('Ошибка сети, попробуйте снова');
+    
+    function showSidebar() {
+        document.querySelector('.side-bar').style.display = 'flex';
     }
-});
 
-
-function saveToLocalStorage(input) {
-    if(input.name) {
-        localStorage.setItem(input.name, input.value);
+    function hideSidebar() {
+        document.querySelector('.side-bar').style.display = 'none';
     }
-}
 
-
-function loadFromLocalStorage() {
-    document.querySelectorAll('input, textarea').forEach(input => {
-        if(input.name && localStorage.getItem(input.name)) {
-            input.value = localStorage.getItem(input.name);
-        }
-    });
-}
-
-
-document.querySelectorAll('input, textarea').forEach(input => {
-    input.addEventListener('input', () => saveToLocalStorage(input));
-});
-
-window.addEventListener('DOMContentLoaded', loadFromLocalStorage);
-
-form.addEventListener('submit', async (e) => {
-    e.preventDefault();
-    const lastPart = document.getElementById('part3');
-
-    const formData = new FormData();
-    lastPart.querySelectorAll('input, textarea').forEach(input => {
-        if(input.value.trim() !== '') {
-            formData.append(input.name, input.value);
-        }
-    });
-    formData.append('access_key', '046ab3ea-c2ac-4eee-a29d-da90b2750ab1');
-
-    try {
-        const response = await fetch('https://api.web3forms.com/submit', {
-            method: 'POST',
-            body: formData
+    
+    const menuBtns = document.querySelectorAll('.menu-btn, .side-bar li');
+    menuBtns.forEach(btn => {
+        btn.addEventListener("click", () => {
+            hideSidebar();
         });
+    });
 
-        const result = await response.json();
+    window.showSidebar = showSidebar; 
+    window.hideSidebar = hideSidebar;
 
-        if (result.success) {
-            document.getElementById('success-message').style.display = 'block';
-            lastPart.querySelector('button[type="submit"]').style.display = 'none';
+   
+    function enableMobileStack() {
+        const container = document.querySelector(".mobile-stack");
+        if (!container) return;
 
-          
-            document.querySelectorAll('input, textarea').forEach(input => {
-                localStorage.removeItem(input.name);
+        let photos = Array.from(container.querySelectorAll("img"));
+        photos.forEach(img => {
+            img.addEventListener("click", () => {
+                let first = photos.shift();
+                photos.push(first);
+                photos.forEach((img, i) => {
+                    img.style.zIndex = photos.length - i;
+                });
             });
-        } else {
-            console.log(result);
-            alert('Ошибка при отправке, попробуйте снова');
-        }
-    } catch (error) {
-        console.error(error);
-        alert('Ошибка сети, попробуйте снова');
+        });
+    }
+
+    if (window.innerWidth <= 540) {
+        enableMobileStack();
     }
 });
